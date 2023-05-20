20b0c10e3afe6f542f9623fd3f86a331b5aab1b8
YasharPa <yasharpashaiy1@gmail.com>
2023-05-20 20:09:04 +0300

    adding edit and remove buttons.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id == id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
   const createBook = (title) => {
     const updatedBooks = [
       { id: Math.round(Math.random() * 9999), title },
@@ -14,7 +31,8 @@ function App() {
   };
   return (
     <div className="app">
-      <BookList books={books} />
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
